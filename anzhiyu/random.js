58a4b983c0509d6483566cc8a84e2f1ff72cdfe7
skycola <zhangweikuo2009@qq.com>
2024-10-01 20:58:28 +0800

    Site updated: 2024-10-01 20:58:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/01/hello-world/"];function toRandomPost(){
+var posts=["2024/10/01/hello-world/","2024/10/01/站点公告/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
