1370c7bafdaf70d03cf4912e6aec767a589b057d
skycola <zhangweikuo2009@qq.com>
2024-10-19 20:42:56 +0800

    Site updated: 2024-10-19 20:42:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/19/51LA网站统计/","2024/10/19/CS中using语句两种用法/","2024/10/19/Everything高效搜索工具/","2024/10/18/Typora安装破解/","2024/10/19/Geek高效卸载工具/","2024/10/19/Windows科学上网/","2024/10/02/测试样式文章test/","2024/10/03/站点说明/"];function toRandomPost(){
+var posts=["2024/10/19/51LA网站统计/","2024/10/19/CS中using语句两种用法/","2024/10/18/Typora安装破解/","2024/10/19/Everything高效搜索工具/","2024/10/02/测试样式文章test/","2024/10/19/Geek高效卸载工具/","2024/10/03/站点说明/","2024/10/19/Windows科学上网/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
